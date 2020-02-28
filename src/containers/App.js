3e7fcbe4f598d77db1b663a34d83012a78211489
Jose Ramirez <josemramirez81@outlook.com>
2020-02-27 22:48:52 -0500

    Project base

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,39 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Route, Switch } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Lists from "./Lists";
+import List from "./List";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppWrapper>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Lists} />
+        <Route path="/list/:id/new" component={Form} />
+        <Route path="/list/:id" component={List} />
+      </Switch>
+    </AppWrapper>
+  </>
+);
 
 export default App;
